b7787349f356ff0f612214f2f0680616569020ac
gatorryan25 <gatorryan25@gmail.com>
2017-05-01 18:06:53 -0400

    Update DisplayNewsActivity.java
    I'm bad at git, so I'm fixing things

--- app/src/main/java/com/example/anthony/thenewsroom/DisplayNewsActivity.java
+++ app/src/main/java/com/example/anthony/thenewsroom/DisplayNewsActivity.java
@@ -1,10 +1,18 @@
 package com.example.anthony.thenewsroom;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,6 +20,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,17 +33,26 @@ public class DisplayNewsActivity extends AppCompatActivity {
     private final String TAG = getClass().getSimpleName();
     private final int TWITTER_CODE = 829;
 
+    public static ListView listView;
     private List headlines;
     private List links;
 
     private ArrayAdapter adapter;
+    private FetchNewsAsyncTask task;
+    private SensorManager sensorManager;
+    private SharedPreferences sharedPref;
+    private float accel; // acceleration apart from gravity
+    private float accelCurrent; // current acceleration including gravity
+    private float accelLast; // last acceleration including gravity
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_news);
-
+        
+        sharedPref = getSharedPreferences("com.example.anthony.thenewsroom", MODE_PRIVATE);
         headlines = new ArrayList();
         links = new ArrayList();
 
@@ -58,6 +77,11 @@ public class DisplayNewsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.registerListener(sensorListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        accel = 0.00f;
+        accelCurrent = SensorManager.GRAVITY_EARTH;
+        accelLast = SensorManager.GRAVITY_EARTH;
     }
 
     @Override
@@ -74,6 +98,9 @@ public class DisplayNewsActivity extends AppCompatActivity {
             case R.id.menu_add_twitter:
                 Intent twitterIntent = new Intent(this, TwitterActivity.class);
                 startActivityForResult(twitterIntent, TWITTER_CODE);
+                return true;
+            case R.id.menu_item_options:
+                startActivity(new Intent(this, Options.class));
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -96,4 +123,34 @@ public class DisplayNewsActivity extends AppCompatActivity {
             adapter.notifyDataSetChanged();
         }
     }
+     private final SensorEventListener sensorListener = new SensorEventListener() {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (sharedPref.getBoolean("Shake", true)){
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                accelLast = accelCurrent;
+                accelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+                float delta = accelCurrent - accelLast;
+                accel = accel * 0.9f + delta; // perform low-cut filter
+                if (accel > 12) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Device has shaken.", Toast.LENGTH_SHORT);
+                    toast.show();
+                    task = new FetchNewsAsyncTask();
+                    task.execute();
+                    toast = Toast.makeText(getApplicationContext(), "Task has been executed", Toast.LENGTH_SHORT);
+                    toast.show();
+
+                }
+            }
+        }
+
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 }
