7bb5ad3abcc9d2f6c3799f9e50473b778cfab8f9
Cameron Jackson <cameronjackson@Camerons-MBP-2.T-mobile.com>
2017-05-01 16:45:25 -0400

    feat: validate and pass back the rss item on successful twitter rss create

--- app/src/main/java/com/example/anthony/thenewsroom/TwitterFragment.java
+++ app/src/main/java/com/example/anthony/thenewsroom/TwitterFragment.java
@@ -1,5 +1,6 @@
 package com.example.anthony.thenewsroom;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,10 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.anthony.thenewsroom.model.RssItem;
 
 import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 public class TwitterFragment extends Fragment {
+
+    public static final String TWITTER_RSS_CREATED = "twitterRssCreated";
 
     private Button mSaveButton;
     private Button mCancelButton;
@@ -53,7 +60,37 @@ public class TwitterFragment extends Fragment {
         mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: Validate and add the rss feed to the database
+
+                String name = mNameTextView.getText().toString();
+                if (name.trim().isEmpty()) {
+                    // name cannot be empty
+                    Toast.makeText(TwitterFragment.this.getContext(), "Name cannot be empty",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String term = mTermTextView.getText().toString();
+                if (term.trim().isEmpty()) {
+                    // term cannot be empty
+                    Toast.makeText(TwitterFragment.this.getContext(), "Name cannot be empty",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                // generate the twitter rss url and create the rss item
+                String twitterUrl = "https://twitrss.me/twitter_search_to_rss/?term=" + term;
+                RssItem rss = new RssItem(name, twitterUrl);
+
+                // create the intent with the new rss feed
+                Intent intent = new Intent();
+                intent.putExtra(TWITTER_RSS_CREATED, rss);
+
+                // end this activity with the OK result
+                getActivity().setResult(RESULT_OK, intent);
+                getActivity().finish();
+
+
             }
         });
 
