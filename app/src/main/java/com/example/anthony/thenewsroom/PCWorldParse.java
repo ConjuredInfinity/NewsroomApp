ff67c37828f8b68e9db4a4a460f0734577c04b96
gatorryan25 <gatorryan25@gmail.com>
2017-05-03 11:21:14 -0400

    Update PCWorldParse.java
    Displays all RSS's in the database

--- app/src/main/java/com/example/anthony/thenewsroom/PCWorldParse.java
+++ app/src/main/java/com/example/anthony/thenewsroom/PCWorldParse.java
@@ -2,6 +2,9 @@ package com.example.anthony.thenewsroom;
 
 import android.util.Log;
 
+import com.example.anthony.thenewsroom.model.RssSource;
+import com.example.anthony.thenewsroom.service.RssService;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -13,6 +16,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import io.realm.Realm;
+import io.realm.internal.Context;
+
 /**
  * Created by Anthony on 4/11/2017.
  */
@@ -20,53 +26,56 @@ import java.util.List;
 public class PCWorldParse {
 
     public static HashMap<String, String> getPCWorldNews() {
-
         List<String> headlines = new ArrayList();
         List<String> links = new ArrayList();
         HashMap<String, String> stories = new HashMap<String, String>();
+        List<RssSource> rssFeeds;
 
-        try {
-            URL url = new URL(RssFeedUrls.PCWorld);
+        rssFeeds = RssService.getRssItems();
+        for (int i = 0; i < rssFeeds.size(); i++) {
+            Log.d("Loading in: ", rssFeeds.get(i).getName());
+            try {
+                URL url = new URL(rssFeeds.get(i).getRssUrl());
 
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(false);
+                XmlPullParser xpp = factory.newPullParser();
 
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            factory.setNamespaceAware(false);
-            XmlPullParser xpp = factory.newPullParser();
+                xpp.setInput(url.openConnection().getInputStream(), "UTF_8");
 
-            xpp.setInput(url.openConnection().getInputStream(), "UTF_8");
+                boolean insideItem = false;
 
-            boolean insideItem = false;
+                int eventType = xpp.getEventType();
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    if (eventType == XmlPullParser.START_TAG) {
 
-            int eventType = xpp.getEventType();
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                if (eventType == XmlPullParser.START_TAG) {
-
-                    if (xpp.getName().equalsIgnoreCase("item")) {
-                        insideItem = true;
-                    } else if (xpp.getName().equalsIgnoreCase("title")) {
-                        if (insideItem)
-                            headlines.add(xpp.nextText());
-                    } else if (xpp.getName().equalsIgnoreCase("link")) {
-                        if (insideItem)
-                            links.add(xpp.nextText());
+                        if (xpp.getName().equalsIgnoreCase("item")) {
+                            insideItem = true;
+                        } else if (xpp.getName().equalsIgnoreCase("title")) {
+                            if (insideItem)
+                                headlines.add(xpp.nextText());
+                        } else if (xpp.getName().equalsIgnoreCase("link")) {
+                            if (insideItem)
+                                links.add(xpp.nextText());
+                        }
+                    } else if (eventType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item")) {
+                        insideItem = false;
                     }
-                }else if(eventType== XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item")){
-                    insideItem=false;
+
+                    eventType = xpp.next();
                 }
 
-                eventType = xpp.next();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Log.i("error boi","ran "+headlines.size());
-        for(int i = 0; i < headlines.size(); i++) {
-            stories.put(headlines.get(i),links.get(i));
+            Log.i("error boi", "ran " + headlines.size());
+            for (int j = 0; j < headlines.size(); j++) {
+                stories.put(headlines.get(j), links.get(j));
+            }
         }
         return stories;
     }
