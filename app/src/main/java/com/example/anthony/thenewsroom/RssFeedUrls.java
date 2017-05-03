510d588c9e1491b873f88ec46b73f1861c8c9bec
gatorryan25 <gatorryan25@gmail.com>
2017-05-03 11:22:22 -0400

    Update RssFeedUrls.java
    Converted to RssSource objects

--- app/src/main/java/com/example/anthony/thenewsroom/RssFeedUrls.java
+++ app/src/main/java/com/example/anthony/thenewsroom/RssFeedUrls.java
@@ -1,21 +1,24 @@
 package com.example.anthony.thenewsroom;
 
+import com.example.anthony.thenewsroom.model.RssSource;
+import com.example.anthony.thenewsroom.service.RssService;
+
 /**
  * This class is where we will put all the urls for the RSS feeds we are using
  */
 
 public class RssFeedUrls {
-    public static final String PCWorld = "http://feeds.pcworld.com/pcworld/latestnews";
-    public static final String Aljazeera = "http://america.aljazeera.com/content/ajam/articles.rss";
-    public static final String ET = "http://entertainmentweekly.tumblr.com/rss";
-    public static final String CNN = "http://rss.cnn.com/rss/cnn_topstories.rss";
-    public static final String THR = "http://feeds.feedburner.com/thr/news";
-    public static final String Bloomberg = "https://www.bloomberg.com/feeds/podcasts/etf_report.xml";
-    public static final String ESPN = "http://www.espn.com/espn/rss/news";
-    public static final String NFL = "http://www.nfl.com/rss/rsslanding?searchString=home";
-    public static final String YahooS = "https://www.yahoo.com/news/rss/sports";
-    public static final String Gizmodo = "http://feeds.gawker.com/gizmodo/full";
-    public static final String NYT = "http://rss.nytimes.com/services/xml/rss/nyt/Books.xml";
-    public static final String Entrepreneur = "http://feeds.feedburner.com/entrepreneur/latest";
-    public static final String Variety = "http://variety.com/static-pages/rss-2/";
+    public static final RssSource pcworld = new RssSource("PCWorld", "http://feeds.pcworld.com/pcworld/latestnews");
+    public static final RssSource aljazeera = new RssSource("Aljazeera", "http://america.aljazeera.com/content/ajam/articles.rss");
+    public static final RssSource et = new RssSource("ET", "http://entertainmentweekly.tumblr.com/rss");
+    public static final RssSource cnn = new RssSource("CNN", "http://rss.cnn.com/rss/cnn_topstories.rss");
+    public static final RssSource thr = new RssSource("THR", "http://feeds.feedburner.com/thr/news");
+    public static final RssSource bloomberg = new RssSource("Bloomberg", "https://www.bloomberg.com/feeds/podcasts/etf_report.xml");
+    public static final RssSource espn = new RssSource("ESPN", "http://www.espn.com/espn/rss/news");
+    public static final RssSource nfl = new RssSource("NFL", "http://www.nfl.com/rss/rsslanding?searchString=home");
+    public static final RssSource yahooS = new RssSource ("YahooS", "https://www.yahoo.com/news/rss/sports");
+    public static final RssSource gizmodo = new RssSource("Gizmodo", "http://feeds.gawker.com/gizmodo/full");
+    public static final RssSource nyt = new RssSource("NYT", "http://rss.nytimes.com/services/xml/rss/nyt/Books.xml");
+    public static final RssSource entrepreneur = new RssSource("Entrepreneur", "http://feeds.feedburner.com/entrepreneur/latest");
+    public static final RssSource variety = new RssSource("Variety", "http://variety.com/static-pages/rss-2/");
 }
